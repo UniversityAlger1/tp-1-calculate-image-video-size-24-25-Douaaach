bf60f0f7346fa524f3376076ed5eea15d4ad3293
achour douaa <achourdouaa14@gmail.com>
2024-11-20 18:57:27 +0100

    solution tp1

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -1,3 +1,5 @@
+
+
 #include "config/grayScaleImage.h"
 // Parameters:
 //   w: width of the image
@@ -5,8 +7,8 @@
 // Return value
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
-   // YOUR CODE HERE - BEGIN
-
-   // YOUR CODE HERE - END
-   return 0;
+    // YOUR CODE HERE - BEGIN
+   // Calculer la taille totale de l'image en octets
+   float size = w *h;
+   return size;
 }
